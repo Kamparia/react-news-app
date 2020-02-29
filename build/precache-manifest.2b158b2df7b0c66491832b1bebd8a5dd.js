ec62edf5794720b0ebf612eb3ab3c901b77bd991
Kamparia <somideolaoye@gmail.com>
2020-02-29 18:31:36 +0000

    Updates

---
+++ build/precache-manifest.2b158b2df7b0c66491832b1bebd8a5dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67c0835674934b18cdd80cc4bd04e87",
+    "url": "/react-news-app/index.html"
+  },
+  {
+    "revision": "6ff6d86370c64d30f203",
+    "url": "/react-news-app/static/css/main.bbb32c1c.chunk.css"
+  },
+  {
+    "revision": "9172295d4b29a4dccfac",
+    "url": "/react-news-app/static/js/2.e6832bc8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-news-app/static/js/2.e6832bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff6d86370c64d30f203",
+    "url": "/react-news-app/static/js/main.5ff3ed80.chunk.js"
+  },
+  {
+    "revision": "4102ed9a15ab9c92319e",
+    "url": "/react-news-app/static/js/runtime-main.309c0c3e.js"
+  }
+]);
