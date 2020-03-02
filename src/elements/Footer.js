b749ae9159ed78cd3b91310579481e501f515111
Kamparia <somideolaoye@gmail.com>
2020-03-02 11:11:07 +0000

    Updates

--- src/elements/Footer.js
+++ src/elements/Footer.js
@@ -7,16 +7,16 @@ class Footer extends Component {
         <div className="container">
           <div className="row">
             <div className="col-sm-6">
-              <p className="mb-1">&copy; Copyright Olaoye Anthony Somide. All Rights Reserved</p>
+              <p className="mb-1">&copy; Copyright <a href="http://somideolaoye.com" rel="noopener noreferrer" target="_blank">Olaoye Anthony Somide</a>. All Rights Reserved</p>
               <div className="credits">
                 Data Source: <a href="https://newsapi.org/" rel="noopener noreferrer" target="_blank">News API</a>
               </div>
             </div>
             <div className="col-sm-6 social text-md-right">
-              <a href="https://linkedin.com"><span className="icofont-linkedin"></span></a>
-              <a href="https://twitter.com"><span className="icofont-twitter"></span></a>
-              <a href="https://facebook.com"><span className="icofont-facebook"></span></a>
-              <a href="https://github.com"><span className="icofont-github"></span></a>
+              <a href="https://linkedin.com/in/kamparia" rel="noopener noreferrer" target="_blank"><span className="icofont-linkedin"></span></a>
+              <a href="https://twitter.com/kamparia" rel="noopener noreferrer" target="_blank"><span className="icofont-twitter"></span></a>
+              <a href="https://facebook.com/kamparia" rel="noopener noreferrer" target="_blank"><span className="icofont-facebook"></span></a>
+              <a href="https://github.com/kamparia" rel="noopener noreferrer" target="_blank"><span className="icofont-github"></span></a>
             </div>
           </div>
         </div>
