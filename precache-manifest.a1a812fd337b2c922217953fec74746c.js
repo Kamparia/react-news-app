b0a22b947e8212fa14b60d48ef7d2a73120bdc0b
Olaoye Somide <somideolaoye@gmail.com>
2021-03-20 01:25:26 +0100

    Updates

---
+++ precache-manifest.a1a812fd337b2c922217953fec74746c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7d2d31bb74030cacec9a876ba924b0",
+    "url": "/react-news-app/index.html"
+  },
+  {
+    "revision": "0a466e074e78177afd98",
+    "url": "/react-news-app/static/css/main.bbb32c1c.chunk.css"
+  },
+  {
+    "revision": "d67bd3a973b55186ab9b",
+    "url": "/react-news-app/static/js/2.f93fa123.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/react-news-app/static/js/2.f93fa123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a466e074e78177afd98",
+    "url": "/react-news-app/static/js/main.0c2e8396.chunk.js"
+  },
+  {
+    "revision": "6ca76c10acc27f28b118",
+    "url": "/react-news-app/static/js/runtime-main.cac37cf4.js"
+  }
+]);
